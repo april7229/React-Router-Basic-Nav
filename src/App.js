13e7f6d77339b7a696ac85023eb5926ce829dd9e
Xang Thao <xangthao24@yahoo.com>
2018-05-07 13:08:31 -0700

    Fixed error

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+import {Home, About, Contact } from './components';
 import { Route, Link } from 'react-router-dom';
 
-const App = () => { 
+const App = () => {
   return (
   <div>
-    < Route exact path='/Home' component={Home}></Route>
+    < Route exact path='/' component={Home}></Route>
     < Route path='/About' component={About}></Route>
     < Route path='/Contact' component={Contact}></Route>
-    
 
-      
-    < Link to="/">Home</Link>
-    <Link to="/About">About</Link>
-    <Link to="/Contact">Contact</Link>
-  
+    <Link to={Home}>Home</Link>
+    <Link to={About}>About</Link>
+    <Link to={Contact}>Contact</Link>
+
     </div >
-  )  
+  )
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import ReactDom from 'react-dom';
-import { BrowserRoute as Route } from 'react-router-dom';
-import { Home, About, Contact, Navigation } from './components'; 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Home, About, Contact, Navigation } from './components';
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>
+  , document.getElementById('root'));
