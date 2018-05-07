c01d49b215daa20b8a4a29e64020037611bab5a8
april <martinezapril56@yahoo.com>
2018-05-07 15:39:21 -0400

    fix me

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route, Link } from 'react-router-dom';
 
-const App = () => (
+const App = () => { 
+  return (
   <div>
-    <Navigation />
-  </div>
-);
+    < Route exact path='/Home' component={Home}></Route>
+    < Route path='/About' component={About}></Route>
+    < Route path='/Contact' component={Contact}></Route>
+    
+
+      
+    < Link to="/">Home</Link>
+    <Link to="/About">About</Link>
+    <Link to="/Contact">Contact</Link>
+  
+    </div >
+  )  
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-
+import ReactDom from 'react-dom';
+import { BrowserRoute as Route } from 'react-router-dom';
+import { Home, About, Contact, Navigation } from './components'; 
 ReactDOM.render(<App />, document.getElementById('root'));
